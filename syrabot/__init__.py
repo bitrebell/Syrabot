20bdac703bb2993306f338f4d9dd040218088846
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-09-30 15:23:37 +0530

    Update __init__.py

--- syrabot/__init__.py
+++ syrabot/__init__.py
@@ -75,7 +75,7 @@ log = Log(True, "bot.log")
 # MongoDB client
 log.info("Initializing MongoDB client")
 mongo_client = MongoClient(MONGO_URL)
-db = mongo_client.wbb
+db = mongo_client.syrabot
 
 
 async def load_sudoers():
